5f7b4c4811fc5cf86dafe30f483289bd0a4e9dca
Charles Palmenta <palmentac@gmail.com>
2020-05-15 21:54:50 -0400

    finished 2 tasks

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -6,4 +6,23 @@
 // under the div.topics element.
 //
 //  Each tab should look like this:
+
 //    <div class="tab">topic here</div>
+const entryPoint = document.querySelector('.topics')
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(response => {
+    console.log('array of topics', response.data.topics)
+    console.log(response)
+
+    response.data.topics.forEach(function(topics) {
+        const newTab = document.createElement('div')
+        newTab.classList.add('tab')
+        newTab.textContent = topics
+        entryPoint.appendChild(newTab)
+    })
+})
+.catch(err => {
+    console.log('ERROR')
+})
+
