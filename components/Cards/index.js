f5ffb72af02a2e125d47b0870b51be8ffe063800
Charles Palmenta <palmentac@gmail.com>
2020-05-18 14:28:42 -0400

    finished javascript

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -21,8 +21,8 @@
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
    .then((response) => {
      console.log(response);
-     let cardsCont = document.querySelector(".cards-container");
-     let arrayValues = Object.values(response.data.articles);
+     const cardsCont = document.querySelector(".cards-container");
+     const arrayValues = Object.values(response.data.articles);
      arrayValues.forEach(element => {
        element.forEach(article => {
          cardsCont.appendChild(createCard(article));
@@ -36,33 +36,30 @@ axios.get('https://lambda-times-backend.herokuapp.com/articles')
 
  function createCard(object){
 
-   const div = document.createElement("div")
-   const div2 = document.createElement("div")
-   const div3 = document.createElement("div")
-   div.classList.add("card");
+   const card = document.createElement("div")
+   const headline = document.createElement("div")
+   const authorContainer = document.createElement("div")
+   const imgContainer = document.createElement("div")
+   const img = document.createElement("img")
+   const authorName = document.createElement("span")
 
+   card.appendChild(headline)
+   card.appendChild(authorContainer)
+   authorContainer.appendChild(imgContainer)
+   imgContainer.appendChild(img)
+   authorContainer.appendChild(authorName)
+
+   card.classList.add("card")
+   headline.classList.add("headline")
+   authorContainer.classList.add("author")
+   imgContainer.classList.add("img-container")
    
-   div2.classList.add("headline");
-   div2.innerHTML = object.headline;
-   div.appendChild(div2);
-
+   headline.textContent = object.headline
+   img.src = object.authorPhoto
+   authorName.textContent = `By ${object.authorName}`
    
-   div3.classList.add("author");
-   div.appendChild(div3);
 
-   let div4 = document.createElement("div");
-   div4.classList.add("img-container");
-   div3.appendChild(div4);
-
-   let img = document.createElement("img");
-   img.src = object.authorPhoto;
-   div4.appendChild(img);
-
-   let span = document.createElement("span");
-   span.innerHTML = "By " + object.authorName;
-   div3.appendChild(span);
-
-   return div;
+   return card;
 
  } 
 
